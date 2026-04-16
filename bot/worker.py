ca6c85f91f24b88d3e5d40e3c28592fb19ac3010
Shubham <wanishubham20@gmail.com>
2026-04-16 18:17:10 +0530

    added print

--- bot/worker.py
+++ bot/worker.py
@@ -10,6 +10,7 @@ from graph.flow import safe_replace
 
 base_dir = Path(__file__).parent.parent.parent
 comfyui_output_path = base_dir / "ComfyUI" / "output"
+print(f"Comfyui Output path: {comfyui_output_path}")
 
 def check_job_status(server_name: str, key: str, name: str, output_path: str,
                      isImage=False, isTransition=False):
